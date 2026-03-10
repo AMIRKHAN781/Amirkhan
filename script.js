dfa40cb94451d3d5c5a50600bfb5184a145bf9b8
AMIRKHAN781 <abaevxan@gmail.com>
2026-03-10 11:56:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 const pages = document.querySelectorAll('.page');
 const btnNext = document.getElementById('next');
 const btnPrev = document.getElementById('prev');
-const counter = document.getElementById('page-counter'); // Добавили счетчик
+const counter = document.getElementById('page-counter');
 let current = 0;
+
+
+function typeWriter(element) {
+    const text = element.innerHTML; 
+    const rawText = element.innerText; 
+    element.innerHTML = ''; 
+    element.style.display = 'block';  
+
+    let i = 0;
+    
+    const speed = 15; 
+
+    function type() {
+        if (i < rawText.length) {
+          
+            element.innerHTML = rawText.substring(0, i + 1);
+            i++;
+            setTimeout(type, speed);
+        } else {
+           
+            element.innerHTML = text;
+        }
+    }
+    type();
+}
 
 function updatePages() {
     pages.forEach((page, index) => {
         if (index === current) {
             page.classList.add('active');
+           
+            typeWriter(page); 
         } else {
             page.classList.remove('active');
         }
     });
 
-    // Обновляем текст счетчика: "Страница 1 из 5"
     if (counter) {
         counter.innerText = `Страница ${current + 1} из ${pages.length}`;
     }
@@ -21,6 +47,7 @@ function updatePages() {
     btnPrev.disabled = (current === 0);
     btnNext.disabled = (current === pages.length - 1);
 }
+
 
 btnNext.addEventListener('click', () => {
     if (current < pages.length - 1) {
@@ -38,14 +65,26 @@ btnPrev.addEventListener('click', () => {
     }
 });
 
-updatePages();
+
 const cover = document.getElementById('book-cover');
 const startBtn = document.getElementById('start-reading');
 
 startBtn.addEventListener('click', () => {
     cover.classList.add('cover-hidden');
-    // Чтобы страница не дергалась, можно добавить задержку удаления из DOM
     setTimeout(() => {
         cover.style.display = 'none';
+        updatePages(); 
     }, 1000);
 });
+
+
+function createHeart(){
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤️";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (20 + Math.random()*20) + "px";
+    document.body.appendChild(heart);
+    setTimeout(() => { heart.remove(); }, 6000);
+}
+setInterval(createHeart, 700);
